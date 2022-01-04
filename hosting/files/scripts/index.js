deee57766d957d1c319e148dc39d035fbf8e3f88
Valeria Viana Gusmao <valeria.viana.gusmao@gmail.com>
2022-01-04 07:01:27 +0100

    fix: hazardous but doesn't enter atmosphere

--- hosting/files/scripts/index.js
+++ hosting/files/scripts/index.js
@@ -2,7 +2,7 @@ import { initApi } from "./api.js"
 import { formatNumber } from './fmt.js'
 
 const exec = await initApi()
-const { data, errors } = await exec(`query($from: Long, $to: Long) {
+const { data } = await exec(`query($from: Long, $to: Long) {
   neos(sortBy: EPOCH_DATE_CLOSE_APPROACH_ASC, query: {epoch_date_close_approach_gt: $from,epoch_date_close_approach_lt: $to} ) {
     _id
 		absolute_magnitude_h
@@ -30,11 +30,11 @@ loader.hidden = true
 let isOk = true
 for (const entry of data.neos) {
   const row = document.createElement('li')
-  const isRowOk = !entry.is_potentially_hazardous_asteroid && entry.miss_distance.kilometers > KARMAN_LINE_KM
+  const isRowOk = parseFloat(entry.miss_distance.kilometers) > KARMAN_LINE_KM
   if (!isRowOk) row.className = 'danger'
   row.innerHTML =
     `<a href="${entry.url}" target="_blank" rel="noopener">${entry.is_sentry_object ? 'Sentry' : 'Asteroid'} ${entry.name}</a> ` +
-    `approaches Earth today at <time>${new Date(Number(entry.epoch_date_close_approach)).toLocaleTimeString()}</time> ` +
+    `approaches Earth today at <time>${new Date(parseFloat(entry.epoch_date_close_approach)).toLocaleTimeString()}</time> ` +
     `at <em>${formatNumber(entry.relative_velocity.kilometers_per_hour)}</em> km/h ` +
     (isRowOk ? `and will miss by <em class="distance">${formatNumber(entry.miss_distance.kilometers)}</em> km.` : `and will enter Earth's atmosphere 😱`)
   list.appendChild(row)
